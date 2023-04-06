48b745f11c1d4d07ca7ce488a8e7209907a69730
darrylamt <dtamoatey001@st.ug.edu.gh>
2023-04-06 11:41:32 -0700

    first push

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints strings followed by a new line
+ * @s - is the input from the user
+ * Return - is always 0
+ */
+
+void _puts_recursion(char *s) {
+	if(*s != '\0'){
+		_putchar(*s);
+		_puts_recursion(s + 1)
+	} else {
+		_putchar('/n')
+	}
+}
