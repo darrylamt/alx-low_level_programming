5389fbc60e21190ea2926477f2e920752a9c6a15
darrylamt <dtamoatey001@st.ug.edu.gh>
2023-04-06 17:52:36 -0700

    done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,47 @@
 #include "main.h"
 
-int check_palindrome(char *string, int start, int end);
-int get_string_length(char *string);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - checks if a string is a palindrome
- * @string: string to check
- * Return: 1 if the string is a palindrome, 0 if not
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
  */
-int is_palindrome(char *string)
+int is_palindrome(char *s)
 {
-	if (*string == '\0')
+	if (*s == 0)
 		return (1);
-	return (check_palindrome(string, 0, get_string_length(string)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * get_string_length - returns the length of a string
- * @string: string to calculate the length of
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
  * Return: length of the string
  */
-int get_string_length(char *string)
+int _strlen_recursion(char *s)
 {
-	if (*string == '\0')
+	if (*s == '\0')
 		return (0);
-	return (1 + get_string_length(string + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_palindrome - checks if the string is a palindrome recursively
- * @string: string to check
- * @start: start index
- * @end: end index
- * Return: 1 if the string is a palindrome, 0 if not
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
  */
-int check_palindrome(char string, int start, int end)
+int check_pal(char *s, int i, int len)
 {
-	if ((string + start) != *(string + end - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
-	if (start >= end)
+	if (i >= len)
 		return (1);
-	return (check_palindrome(string, start + 1, end - 1));
+	return (check_pal(s, i + 1, len - 1));
 }
