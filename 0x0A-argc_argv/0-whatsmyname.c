881e2e1774f6920c6c48de68b256874c51281641
darrylamt <dtamoatey001@st.ug.edu.gh>
2023-04-07 12:01:33 -0700

    try again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - this is our main function for printing the name of the file
+ * @argc: give the total index of the commands used at compilation
+ * @argv: gives the exact commands used at compilation
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", *argv[i]);
 	}
 
-	return 0;
+	return (0);
 }
