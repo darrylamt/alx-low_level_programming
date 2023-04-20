947206332257791cb86d85a3ccc7b43ad799e4e9
darrylamt <dtamoatey001@st.ug.edu.gh>
2023-04-20 16:33:13 -0700

    all done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 
 /**
  * get_op_func - Selects the correct function to perform
- * 
+ *               the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
